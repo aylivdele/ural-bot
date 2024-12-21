14fad9b1bad351085fec3b6954d529624de0595c
aylivdele <irassomahin@gmail.com>
2024-12-22 00:55:44 +0700

    init

---
+++ index.js
@@ -0,0 +1,23 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+require('dotenv').config()
+
+
+const token = process.env.API_TOKEN
+const url = process.env.URL
+
+const bot = new TelegramBot(token, {
+    webHook: {
+        port: process.env.PORT,
+        autoOpen: false,
+    }
+})
+
+if (token && url) {
+    bot.openWebHook();
+    bot.setWebHook(`${url}/bot${token}`)
+}
+
+bot.on('message', msg => {
+    bot.sendMessage(msg.chat.id, `Reply to '${ msg.text }'`, { reply_to_message_id: msg.message_id});
+})
